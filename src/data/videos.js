d1394e260b6bbe1829ba2d3cea15850f3b0fd0ec
Joey Robinson <theamazingness@gmail.com>
2019-02-05 00:09:10 -0600

    Added video array for dummy api calls. duration increments by 1s just to stay in line with the name and title

---
+++ src/data/videos.js
@@ -0,0 +1,127 @@
+const videos = [
+  {
+    name: "video-one", 
+    title: "Description for video-one", 
+    duration: 1
+  },
+  {
+    name: "video-two", 
+    title: "Description for video-two", 
+    duration: 2
+  },
+  {
+    name: "video-three", 
+    title: "Description for video-three", 
+    duration: 3
+  },
+  {
+    name: "video-four", 
+    title: "Description for video-four", 
+    duration: 4
+  },
+  {
+    name: "video-five", 
+    title: "Description for video-five", 
+    duration: 5
+  },
+  {
+    name: "video-six", 
+    title: "Description for video-six", 
+    duration: 6
+  },
+  {
+    name: "video-seven", 
+    title: "Description for video-seven", 
+    duration: 7
+  },
+  {
+    name: "video-eight", 
+    title: "Description for video-eight", 
+    duration: 8
+  },
+  {
+    name: "video-nine", 
+    title: "Description for video-nine", 
+    duration: 9
+  },
+  {
+    name: "video-ten", 
+    title: "Description for video-ten", 
+    duration: 10
+  },
+  {
+    name: "video-eleven", 
+    title: "Description for video-eleven", 
+    duration: 11
+  },
+  {
+    name: "video-twelve", 
+    title: "Description for video-twelve", 
+    duration: 12
+  },
+  {
+    name: "video-thirteen", 
+    title: "Description for video-thirteen", 
+    duration: 13
+  },
+  {
+    name: "video-fourteen", 
+    title: "Description for video-fourteen", 
+    duration: 14
+  },
+  {
+    name: "video-fifteen", 
+    title: "Description for video-fifteen", 
+    duration: 15
+  },
+  {
+    name: "video-sixteen", 
+    title: "Description for video-sixteen", 
+    duration: 16
+  },
+  {
+    name: "video-seventeen", 
+    title: "Description for video-seventeen", 
+    duration: 17
+  },
+  {
+    name: "video-eighteen", 
+    title: "Description for video-eighteen", 
+    duration: 18
+  },
+  {
+    name: "video-nineteen", 
+    title: "Description for video-nineteen", 
+    duration: 19
+  },
+  {
+    name: "video-twenty", 
+    title: "Description for video-twenty", 
+    duration: 20
+  },
+  {
+    name: "video-twentyone", 
+    title: "Description for video-twentyone", 
+    duration: 21
+  },
+  {
+    name: "video-twentytwo", 
+    title: "Description for video-twentytwo", 
+    duration: 22
+  },
+  {
+    name: "video-twentythree", 
+    title: "Description for video-twentythree", 
+    duration: 23
+  },
+  {
+    name: "video-twentyfour", 
+    title: "Description for video-twentyfour", 
+    duration: 24
+  },
+  {
+    name: "video-twentyfive", 
+    title: "Description for video-twentyfive", 
+    duration: 25
+  }
+]
