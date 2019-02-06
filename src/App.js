e02bfbf2734f091823d2b364bbdc696103e95da1
Paul <azrael7@gmail.com>
2019-02-06 08:49:04 -0600

    Added test thumbnail, eslintrc update, added properties to videos.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React, { Component } from 'react';
-// import './App.css';
-import { videos } from './data/videos';
-import {listStyle, divGeneral } from './style'
+import React, { Component } from 'react'
+import { videos } from './data/videos'
+import { listStyle, divGeneral } from './style'
+import thumbnailSrc from './images/moon.jpg'
 
 class App extends Component {
   render() {
     return (
       <div className="App" style={divGeneral}>
-          <ul style={listStyle}>
-            {/* Todo: Add an identifier to the video array so the className isn't awkward */}
+        <ul style={listStyle}>
+          {/* Todo: Add an identifier to the video array so the className isn't awkward */}
           {videos.map((video, index) => (
-            <li 
-              className={video.name}
-              key={index}
-            >
+            <li className={video.name} key={index}>
+              <img src={thumbnailSrc} alt="thumbnail" />
               {video.name}
+              <div>{video.channelName}</div>
+              <div>
+                {video.views} views - {video.duration} hours ago
+              </div>
             </li>
           ))}
-          </ul>
+        </ul>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
--- src/style.js
+++ src/style.js
@@ -3,11 +3,11 @@ export const listStyle = {
   color: 'white',
   backgroundColor: 'inherit',
   display: 'grid',
-  gridTemplateColumns: 'repeat(auto-fill, minmax(10rem, 15rem))',
+  gridTemplateColumns: 'repeat(auto-fill, minmax(10rem, 22rem))',
   listStyle: 'none',
-  padding: '1rem 2rem'
+  padding: '1rem 2rem',
 }
 
 export const divGeneral = {
-  backgroundColor: '#2F4F4F'
+  backgroundColor: '#2F4F4F',
 }
